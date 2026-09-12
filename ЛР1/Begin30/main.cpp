70467b79f2828690eabc709379a3a20758fbec03
haisachenko <h.a.isachenko@student.khai.edu>
2026-09-12 16:47:22 +0000

    Create main.cpp

---
+++ ЛР1/Begin30/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Задача Begin 30
+    // Змінні  третьої задачі
+    double a, b, S;
+    // Введення змінних
+    cout<<"Сторона А: ";
+    cin >>a;
+    cout<<"Сторона Б: ";
+    cin >>b;
+    // Розрахунок площі
+    S = (a*b)/2;
+    // Виведення результатів
+    cout<<"Площа трикутника: " <<S<<endl;
+
+    return 0;
+}
